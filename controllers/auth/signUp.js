f6430932dcd4eb39ddd3bdade1413bd6a1b1f8bc
Artem Mironov <31404136+artemko77788@users.noreply.github.com>
2022-08-20 20:00:36 +0300

    add

--- controllers/auth/signUp.js
+++ controllers/auth/signUp.js
@@ -1,6 +1,9 @@
 const asyncHandler = require('express-async-handler')
+const {User} =require('../../models')
 const reateErrors = require('http-errors')
 
-const signUp = asyncHandler(async (req, res) => {})
+const signUp = asyncHandler(async (req, res) => {
+    const {name, email, password}= req.body
+})
 
 module.exports = signUp
